477db0f4d84aa065a8147bde555c77636f230754
Jaesuru <jasongms2017@gmail.com>
2025-03-11 19:29:26 -0400

    Initial Commit

---
+++ PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by Jason Jiang on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

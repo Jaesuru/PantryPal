5fbc0b163778e795903291d73b51f31758febaa1
Jaesuru <jasongms2017@gmail.com>
2025-04-01 11:27:02 -0400

    Perma light mode, woo

--- PantryPal/App/PantryPalApp.swift
+++ PantryPal/App/PantryPalApp.swift
@@ -10,10 +10,13 @@ import ParseSwift
 
 @main
 struct PantryPalApp: App {
+    
     init() {
         ParseSwift.initialize(applicationId: "nhJFy7RKNyvOXLY5FpuF0E9kpQJ614SQLkD0IQqq",
                               clientKey: "4zdYq91YAFCvM6ZDz1e49bVHDpSvHrNuqv0jbtG4",
                               serverURL: URL(string: "https://parseapi.back4app.com")!)
+        
+        UIView.appearance().overrideUserInterfaceStyle = .light
     }
     
     var body: some Scene {
